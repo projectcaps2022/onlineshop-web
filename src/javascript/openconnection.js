c3da0ce75eb6a43191ecb258d35d326939a46c24
projectcaps2022 <projectcaps2022@gmail.com>
2022-01-23 17:04:51 -0500

    setting sql.js

--- src/javascript/openconnection.js
+++ src/javascript/openconnection.js
@@ -1,6 +1,4 @@
 
-
-
 function gettest(){
     try{
 var connection = new ActiveXObject("ADODB.Connection") ;
@@ -93,4 +91,23 @@ function deletetest(id){
   //  rs.close;
     connection.close; 
     gettest()
+}
+
+function testSQLJS() {
+    var db = new SQL.Database();
+    // Run a query without reading the results
+    db.run("CREATE TABLE test (col1, col2);");
+    // Insert two rows: (1,111) and (2,222)
+    db.run("INSERT INTO test VALUES (?,?), (?,?)", [1,111,2,222]);
+
+    // Prepare a statement
+    var stmt = db.prepare("SELECT * FROM test WHERE col1 BETWEEN $start AND $end");
+    stmt.getAsObject({$start:1, $end:1}); // {col1:1, col2:111}
+
+    // Bind new values
+    stmt.bind({$start:1, $end:2});
+    while(stmt.step()) { //
+        var row = stmt.getAsObject();
+        // [...] do something with the row of result
+    }
 }
