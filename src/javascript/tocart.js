0cd319dcde319b0564dd80e2273a5b8901b98b27
projectcaps2022 <projectcaps2022@gmail.com>
2022-01-29 18:26:21 -0500

    buy and cart

---
+++ src/javascript/tocart.js
@@ -0,0 +1,9 @@
+function showcart(product=null){
+
+    const modalCaartContent = document.getElementById('exampleSideModal1')
+    const modalCart = new mdb.Modal(modalCaartContent)
+    
+
+    modalCart.show()
+
+}
