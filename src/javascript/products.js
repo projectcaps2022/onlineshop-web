f027bcf74d1578df39b1a074437f1daa9f8f8c7e
projectcaps2022 <projectcaps2022@gmail.com>
2022-01-30 20:04:14 -0500

    get products woman and man

--- src/javascript/products.js
+++ src/javascript/products.js
@@ -2,7 +2,10 @@
 
 
 function getproducts(){
-
+    	
+    const prodmod = document.getElementById('loadingprods')
+    const modalp = new mdb.Modal(prodmod)
+    modalp.show()
     
     // <ul class="rating" data-mdb-toggle="rating" data-mdb-readonly="true" data-mdb-value="3">
     //     <li>
@@ -73,6 +76,9 @@ function getproducts(){
                                 '<span>Color</span><span>'+element[4]+'</span>'+
                              ' </div>'+
                              ' <div class="d-flex justify-content-between">'+
+                                '<span>Dress</span><span>'+element[2]+'</span>'+
+                             ' </div>'+
+                             ' <div class="d-flex justify-content-between">'+
                                 '<span>Rating</span>'+'<i class="fa-star fa-sm fas active" style="color: #FBD20E;">'+element[6]+'</i>'+
                              ' </div>'+
                              '</div>'+
@@ -89,6 +95,221 @@ function getproducts(){
                        
                         
                     }
+                         modalp.hide()
+
+                    divproduct.innerHTML = texthtmlprod
+               }
+            )
+          
+      
+        
+      });
+}
+
+
+function getproductsWoman(){
+
+    
+    // <ul class="rating" data-mdb-toggle="rating" data-mdb-readonly="true" data-mdb-value="3">
+    //     <li>
+    //       <i class="fa-star fa-sm text-primary fas active"></i>
+    //     </li>
+    //     <li>
+    //       <i class="fa-star fa-sm text-primary fas active"></i>
+    //     </li>
+    //     <li>
+    //       <i class="fa-star fa-sm text-primary fas active"></i>
+    //     </li>
+    //     <li>
+    //       <i class="far fa-star fa-sm text-primary"></i>
+    //     </li>
+    //     <li>
+    //       <i class="far fa-star fa-sm text-primary"></i>
+    //     </li>
+    //   </ul>
+    const prodmod = document.getElementById('loadingprods')
+    const modalp = new mdb.Modal(prodmod)
+    modalp.show()
+
+
+
+    let url = "https://script.google.com/macros/s/AKfycbykIiDHELoKG7uGBrcRboxo9JodDV7KR9MRO_HufNr3J61Vy3AObD7ZNfIO3KrzzAtz/exec"
+  
+    var a ;  
+    fetch(url, {
+        method: 'GET'
+      })
+      .then((response) => {
+
+          a = response.clone();
+
+
+          var text = ''
+            a.json().then((data) => {
+                    console.log(data)
+                    //var products = []
+                    var texthtmlprod = ''
+                    const divproduct = document.getElementById("productlist")
+                    var rating ='<ul class="rating" data-mdb-toggle="rating" data-mdb-readonly="true" data-mdb-value="3"> '+
+                    '<li> '+
+                    '     <i class="fa-star fa-sm text-primary fas active"></i> '+
+                    '  </li> '+
+                    '   <li> '
+                    ' <i class="fa-star fa-sm text-primary fas active"></i> '+
+                    ' </li> '+
+                    '  <li> '+
+                    '  <i class="fa-star fa-sm text-primary fas active"></i> '+
+                    '</li> '+
+                    '  <li> '+
+                    '    <i class="far fa-star fa-sm text-primary"></i> '+
+                    '  </li> '+
+                    '  <li> '+
+                    ' <i class="far fa-star fa-sm text-primary"></i> '+
+                      ' </li> '+
+                  '</ul>'
+
+                    for (let index = 1; index < data.row.length; index++) {
+                        const element = data.row[index];
+                        if(element[1]==="Woman"){
+                        texthtmlprod+= '<div class="col"><div class="card text-black"><img src="'+element[5]+'" class="card-img-top" alt="..."/> <div class="card-body"> <div class="text-center">'+
+                              '<h5 class="card-title">'+element[0]+'</h5>'+
+                              '<p class="text-muted mb-4">'+element[3]+'</p>'
+                            +'</div>'+
+                            '<div>'+
+                              '<div class="d-flex justify-content-between">'+
+                               ' <span>Category</span><span>'+element[1]+'</span>'+
+                              '</div>'+
+                             ' <div class="d-flex justify-content-between">'+
+                                '<span>Color</span><span>'+element[4]+'</span>'+
+                             ' </div>'+
+                             ' <div class="d-flex justify-content-between">'+
+                                '<span>Dress</span><span>'+element[2]+'</span>'+
+                             ' </div>'+
+                             ' <div class="d-flex justify-content-between">'+
+                                '<span>Rating</span>'+'<i class="fa-star fa-sm fas active" style="color: #FBD20E;">'+element[6]+'</i>'+
+                             ' </div>'+
+                             '</div>'+
+                             ' <div class="d-flex justify-content-between total font-weight-bold mt-4">'+
+                             ' <span>Price</span><span>$'+Number.parseFloat(element[7]).toFixed(2)+'</span>'+
+                             '</div>'+
+                             ' </div>'+
+                             '<div class="card-footer text-center">'+
+                             ' <button type="button" class="btn btn-outline-primary" data-mdb-ripple-color="dark" style="border-color: #dd3675; color: #dd3675;" onclick="showcart()">Add to Cart</button>'+
+                
+                             ' </div>'+
+                             '</div>'+
+                             '</div>'
+                        }
+                        
+                    }
+                    modalp.hide()
+
+                    divproduct.innerHTML = texthtmlprod
+               }
+            )
+          
+      
+        
+      });
+}
+
+
+function getproductsMEn(){
+
+    const prodmod = document.getElementById('loadingprods')
+    const modalp = new mdb.Modal(prodmod)
+    modalp.show()
+    // <ul class="rating" data-mdb-toggle="rating" data-mdb-readonly="true" data-mdb-value="3">
+    //     <li>
+    //       <i class="fa-star fa-sm text-primary fas active"></i>
+    //     </li>
+    //     <li>
+    //       <i class="fa-star fa-sm text-primary fas active"></i>
+    //     </li>
+    //     <li>
+    //       <i class="fa-star fa-sm text-primary fas active"></i>
+    //     </li>
+    //     <li>
+    //       <i class="far fa-star fa-sm text-primary"></i>
+    //     </li>
+    //     <li>
+    //       <i class="far fa-star fa-sm text-primary"></i>
+    //     </li>
+    //   </ul>
+
+
+
+    let url = "https://script.google.com/macros/s/AKfycbykIiDHELoKG7uGBrcRboxo9JodDV7KR9MRO_HufNr3J61Vy3AObD7ZNfIO3KrzzAtz/exec"
+  
+    var a ;  
+    fetch(url, {
+        method: 'GET'
+      })
+      .then((response) => {
+
+          a = response.clone();
+
+
+          var text = ''
+            a.json().then((data) => {
+                    console.log(data)
+                    //var products = []
+                    var texthtmlprod = ''
+                    const divproduct = document.getElementById("productlist")
+                    var rating ='<ul class="rating" data-mdb-toggle="rating" data-mdb-readonly="true" data-mdb-value="3"> '+
+                    '<li> '+
+                    '     <i class="fa-star fa-sm text-primary fas active"></i> '+
+                    '  </li> '+
+                    '   <li> '
+                    ' <i class="fa-star fa-sm text-primary fas active"></i> '+
+                    ' </li> '+
+                    '  <li> '+
+                    '  <i class="fa-star fa-sm text-primary fas active"></i> '+
+                    '</li> '+
+                    '  <li> '+
+                    '    <i class="far fa-star fa-sm text-primary"></i> '+
+                    '  </li> '+
+                    '  <li> '+
+                    ' <i class="far fa-star fa-sm text-primary"></i> '+
+                      ' </li> '+
+                  '</ul>'
+
+                    for (let index = 1; index < data.row.length; index++) {
+                        const element = data.row[index];
+                        if(element[1]==="Men"){
+                        texthtmlprod+= '<div class="col"><div class="card text-black"><img src="'+element[5]+'" class="card-img-top" alt="..."/> <div class="card-body"> <div class="text-center">'+
+                              '<h5 class="card-title">'+element[0]+'</h5>'+
+                              '<p class="text-muted mb-4">'+element[3]+'</p>'
+                            +'</div>'+
+                            '<div>'+
+                              '<div class="d-flex justify-content-between">'+
+                               ' <span>Category</span><span>'+element[1]+'</span>'+
+                              '</div>'+
+                             ' <div class="d-flex justify-content-between">'+
+                                '<span>Color</span><span>'+element[4]+'</span>'+
+                             ' </div>'+
+                             ' <div class="d-flex justify-content-between">'+
+                                '<span>Dress</span><span>'+element[2]+'</span>'+
+                             ' </div>'+
+                             ' <div class="d-flex justify-content-between">'+
+                                '<span>Rating</span>'+'<i class="fa-star fa-sm fas active" style="color: #FBD20E;">'+element[6]+'</i>'+
+                             ' </div>'+
+                             '</div>'+
+                             ' <div class="d-flex justify-content-between total font-weight-bold mt-4">'+
+                             ' <span>Price</span><span>$'+Number.parseFloat(element[7]).toFixed(2)+'</span>'+
+                             '</div>'+
+                             ' </div>'+
+                             '<div class="card-footer text-center">'+
+                             ' <button type="button" class="btn btn-outline-primary" data-mdb-ripple-color="dark" style="border-color: #dd3675; color: #dd3675;" onclick="showcart()">Add to Cart</button>'+
+                
+                             ' </div>'+
+                             '</div>'+
+                             '</div>'
+                        }
+                        
+                    }
+                    modalp.hide()
+
                     divproduct.innerHTML = texthtmlprod
                }
             )
