02804b9ea3cdc453c8b7b79054118a1d5931769d
projectcaps2022 <projectcaps2022@gmail.com>
2022-02-12 20:30:39 -0500

    update context

--- src/javascript/guard.js
+++ src/javascript/guard.js
@@ -53,3 +53,52 @@ function getItemsCart () {
        
      });
  }
+
+ function getItemsCart2 (isenabled=false) {
+ 
+  
+  // const prodmod = document.getElementById('loadingprods')
+  // const modalp = new mdb.Modal(prodmod)
+  // modalp.show()
+   
+    let users = localStorage.getItem('user')
+    var badge = document.getElementById('badgecart');
+    ////console.log(users);
+   let url = "https://script.google.com/macros/s/AKfycbxvQT_14qkxm2eZt7Y6qtn_r5klZrbhIBfgyKr9sbqxGswvpBsCMjmj4bqbwBxUrRygaA/exec"
+ 
+   var a ;  
+   fetch(url, {
+       method: 'GET'
+     })
+     .then((response) => {
+ 
+         a = response.clone();
+ 
+ 
+         var text = ''
+           a.json().then((data) => {
+                 //  //console.log(data)
+                   var cart = []
+
+                   for (let index = 0; index < data.row.length; index++) {
+                     const element = data.row[index];
+                    if(element[4]=== users && element[1]==="Pending"){
+                    
+                      cart.push(element)
+
+                    }
+                   }
+                   localStorage.removeItem('usercart')
+                   localStorage.setItem('usercart', JSON.stringify(cart))
+                   badge.innerHTML = cart.length
+                  //  if(isenabled){
+                  //   buildProds()
+                  //  }
+                  //  modalp.hide()
+              }
+           )
+         
+     
+       
+     });
+ }
